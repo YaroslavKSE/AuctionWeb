32b99023c2c3ea6a738aab44fcbc2e5581ede78c
YaroslavKSE <ikhomych@kse.org.ua>
2024-06-03 14:40:56 +0300

    Setup mongo db and containerized app

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return "Hello, MongoDB!"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
